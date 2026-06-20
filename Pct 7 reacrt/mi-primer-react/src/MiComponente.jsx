48d5ce2207b9023fa9766d77887b5e88467ae29a
keralo2 <keyli.arevalo2026@frech.superate.org>
2026-06-19 18:58:17 -0600

    Practica 7 terminada.

---
+++ Pct 7 reacrt/mi-primer-react/src/MiComponente.jsx
@@ -0,0 +1,10 @@
+
+
+export default function MiComponente({titulo, lugar}) {
+    return(
+        <section className="card">
+        <h2>ClassName="card-title">{titulo}</h2>
+        <h3>className="card-subtitle">{lugar}</h3>
+        </section>
+    )
+}
